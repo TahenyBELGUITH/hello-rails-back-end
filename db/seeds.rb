03360d7d3e3211b460df087c48efe4aab0d778aa
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-11-30 08:39:34 +0100

    Add rails part

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+Greeting.create(message: 'Hello Touha!')
+Greeting.create(message: 'Hello Abel!')
+Greeting.create(message: 'Hello Said!')
+Greeting.create(message: 'Hello Ashour!')
+Greeting.create(message: 'Hello Techie!')
